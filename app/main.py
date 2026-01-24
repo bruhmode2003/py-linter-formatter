d7e3a6ffd7087d95380761042be34ac1f4a426dd
Yuriy Vorobiov <vorobjovura@gmail.com>
2026-01-24 23:53:17 +0200

    edit eroors line 13

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ def format_linter_error(error: dict) -> dict:
         "line": error["line_number"],
         "column": error["column_number"],
         "message": error["text"],
-        "name": error["code"],
+        "name": error["e"],
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [format_linter_error(error) for error in errors],
+        "errors": [format_linter_error(ecodrror) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
